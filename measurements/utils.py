8280b644477b66c759d52992c5f2ca96932f1deb
Guilherme Augusto <guilherme.francais@gmail.com>
2019-09-11 15:58:06 -0300

    Updates method to get environment variables

--- measurements/utils.py
+++ measurements/utils.py
@@ -2,13 +2,13 @@ import os
 import requests
 from .models import Measurement
 
-TOKEN = 'c9a3d9450858418edd708a73d631cb4b'
-API_URL = 'http://api.openweathermap.org/data/2.5/find?appid={}'.format(TOKEN)
+TOKEN = os.getenv('TOKEN', '')
+API_URL = os.getenv('API_URL', '')
 
 class DataCollector():
     def get_specific_data(self, latitude, longitude):
         location = '&lat={}&lon={}'.format(latitude, longitude)
-        response = requests.get(API_URL + location).json()
+        response = requests.get(API_URL + TOKEN + location).json()
         return response['list']
 
     def save_data(self, latitude, longitude):
