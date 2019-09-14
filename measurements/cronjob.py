19d0ba21b8c56c003a56962457a9a9541e3ff25f
Guilherme Augusto <guilherme.francais@gmail.com>
2019-09-14 07:06:36 -0300

    Creates cronjob to catch all forecast measurements

--- measurements/cronjob.py
+++ measurements/cronjob.py
@@ -11,3 +11,12 @@ class CollectMeasurementCronJob(CronJobBase):
     def do(self):
         data_collector = DataCollector()
         data_collector.collect_minutely_measurements()
+
+class CollectForecastCronJob(CronJobBase):
+    RUN_EVERY_MINS = 0
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code = 'measurements.cronjob.CollectForecastCronJob'
+
+    def do(self):
+        data_collector = DataCollector()
+        data_collector.collect_forecast_measurements()
--- measurements/utils.py
+++ measurements/utils.py
@@ -1,29 +1,46 @@
 import os
 import requests
 from .models import Measurement
+from .models import ForecastMeasurement
 from locations.models import Location
 
 TOKEN = os.getenv('TOKEN_ID', '')
 API_URL = os.getenv('API_URL', '')
+FORECAST_URL = os.getenv('FORECAST_URL', '')
 
 class DataCollector():
-    def get_specific_data(self, latitude, longitude):
+    def get_specific_data(self, latitude, longitude, url):
         location = '&lat={}&lon={}'.format(latitude, longitude)
-        response = requests.get(API_URL + TOKEN + location).json()
+        response = requests.get(url + TOKEN + location).json()
         print("Get specific data: {}".format(API_URL + TOKEN + location))
-        return response['list'][0]
+        return response['list']
 
-    def save_data(self, location):
-        print("Save data")
+    def save_minutelly_data(self, location):
         response = self.get_specific_data(
             location.latitude,
-            location.longitude
+            location.longitude,
+            API_URL
         )
-        measurement = Measurement()
-        measurement.save_measurements(location, response)
+        measurement = MinutelyMeasurement()
+        measurement.save_measurements(location, response[0])
+
+    def save_forecast_data(self, location):
+        response = self.get_specific_data(
+            location.latitude,
+            location.longitude,
+            FORECAST_URL
+        )
+        measurement = ForecastMeasurement
+        measurement.save_measurements(location, response[0:7])
 
     def collect_minutely_measurements(self):
         locations = Location.objects.all()
 
         for location in locations:
-            self.save_data(location)
+            self.save_minutelly_data(location)
+
+    def collect_forecast_measurements(self):
+        locations = Location.objects.all()
+
+        for location in locations:
+            self.save_forecast_data(location)
