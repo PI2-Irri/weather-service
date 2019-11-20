f197a760997bd26e141e6634f947b1705682ac0b
Bruna Pinos <bruna.pinos@gmail.com>
2019-11-20 16:55:53 -0300

    Register location

--- locations/serializers.py
+++ locations/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Location
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.exceptions import APIException
 
 
 class LocationSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +14,22 @@ class LocationSerializer(serializers.HyperlinkedModelSerializer):
             'longitude',
             'url'
         )
+   
+    def create(self, validated_data):
+
+        try:
+            location = Location.objects.get(location_name=validated_data.get('location_name'))
+        except Location.DoesNotExist:
+
+            try:
+                location = Location.objects.create(
+                    location_name=validated_data.get('location_name'),
+                    latitude=validated_data.get('latitude'),
+                    longitude=validated_data.get('longitude')
+                )
+            except Exception:
+                raise APIException(
+                    {'error': 'Could not register'}
+                )
+
+        return location
